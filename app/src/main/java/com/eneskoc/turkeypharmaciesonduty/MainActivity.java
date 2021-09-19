557a15f4ccaa306085b8044397f234fcf78187c2
Enes Koç <enskoc97@gmail.com>
2021-09-19 13:04:43 +0300

    Design fixes completed, api model and endpoint added (Feature/03)

--- app/src/main/java/com/eneskoc/turkeypharmaciesonduty/MainActivity.java
+++ app/src/main/java/com/eneskoc/turkeypharmaciesonduty/MainActivity.java
@@ -1,14 +1,110 @@
 package com.eneskoc.turkeypharmaciesonduty;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.eneskoc.turkeypharmaciesonduty.model.ProvinceModel;
+import com.eneskoc.turkeypharmaciesonduty.model.PharmacyModel;
+import com.eneskoc.turkeypharmaciesonduty.service.ApiClient;
+import com.eneskoc.turkeypharmaciesonduty.service.ApiInterface;
+import com.eneskoc.turkeypharmaciesonduty.databinding.ActivityMainBinding;
+import com.eneskoc.turkeypharmaciesonduty.utils.JsonUtils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+
+    private final List<String> citiesList = new ArrayList<>();
+    private List<ProvinceModel> provinceModels = new ArrayList<>();
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+
+        String jsonFileString = JsonUtils.getJsonFromAssets(getApplicationContext(), "turkey_provinces_and_districts.json");
+        Log.i("data", jsonFileString);
+
+        Gson gson = new Gson();
+        Type listUserType = new TypeToken<List<ProvinceModel>>() {}.getType();
+
+        provinceModels = gson.fromJson(jsonFileString, listUserType);
+        for (int i = 0; i < provinceModels.size(); i++) {
+            citiesList.add(provinceModels.get(i).getIl());
+            Log.i("data", i + "::" + provinceModels.get(i).getIl()+ "--" + provinceModels.get(i).getPlaka()+ "\n" + provinceModels.get(i).getIlceleri());
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.spinner_list_item,
+                citiesList);
+        mainBinding.spinnerCity.setAdapter(arrayAdapter);
+        mainBinding.spinnerCity.setOnItemSelectedListener(this);
+
+
+        mainBinding.tvSearchBtn.setOnClickListener(v -> {
+            int cityIndexValue = mainBinding.spinnerCity.getSelectedItemPosition();
+            List<String> ilceler = provinceModels.get(cityIndexValue).getIlceleri();
+            int districtsIndexValue = mainBinding.spinnerDistrict.getSelectedItemPosition();
+            String districtsStringValue = ilceler.get(districtsIndexValue);
+
+            ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+            Call<List<PharmacyModel>> call = apiInterface.getPharmacies(cityIndexValue, districtsStringValue,0);
+            call.enqueue(new Callback<List<PharmacyModel>>() {
+                @Override
+                public void onResponse(Call<List<PharmacyModel>> call, Response<List<PharmacyModel>> response) {
+
+                    List<PharmacyModel> responseBody = response.body();
+                    Log.i("TAG", "onResponse: "+responseBody);
+                    System.out.println("LİSTE:"+responseBody);
+                    System.out.println("0-->"+responseBody.get(0).getEczaneAdi());
+                    System.out.println("1-->"+responseBody.get(1).getEczaneAdi());
+                    System.out.println("2-->"+responseBody.get(2).getEczaneAdi());
+
+                }
+
+                @Override
+                public void onFailure(Call<List<PharmacyModel>> call, Throwable t) {
+
+                }
+            });
+        });
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        int cityIndexValue = mainBinding.spinnerCity.getSelectedItemPosition();
+        List<String> ilceler = provinceModels.get(cityIndexValue).getIlceleri();
+
+        ArrayAdapter<String> arrayIlceAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.spinner_list_item,
+                ilceler);
+        mainBinding.spinnerDistrict.setAdapter(arrayIlceAdapter);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ app/src/main/java/com/eneskoc/turkeypharmaciesonduty/model/PharmacyModel.java
@@ -0,0 +1,176 @@
+package com.eneskoc.turkeypharmaciesonduty.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PharmacyModel {
+
+    @SerializedName("eczaneAdi")
+    @Expose
+    private String eczaneAdi;
+
+    @SerializedName("eczaneIlAdi")
+    @Expose
+    private String eczaneIlAdi;
+
+    @SerializedName("eczaneIlceAdi")
+    @Expose
+    private String eczaneIlceAdi;
+
+    @SerializedName("eczaneAdres")
+    @Expose
+    private String eczaneAdres;
+
+    @SerializedName("tarih")
+    @Expose
+    private String tarih;
+
+    @SerializedName("baslangic")
+    @Expose
+    private String baslangic;
+
+    @SerializedName("bitis")
+    @Expose
+    private String bitis;
+
+    @SerializedName("enlem")
+    @Expose
+    private String enlem;
+
+    @SerializedName("boylam")
+    @Expose
+    private String boylam;
+
+    @SerializedName("glnNo")
+    @Expose
+    private String glnNo;
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("olusturmaTarihi")
+    @Expose
+    private String olusturmaTarihi;
+
+    @SerializedName("guncellemeTarihi")
+    @Expose
+    private Object guncellemeTarihi;
+
+    @SerializedName("silindi")
+    @Expose
+    private Object silindi;
+
+    public String getEczaneAdi() {
+        return eczaneAdi;
+    }
+
+    public void setEczaneAdi(String eczaneAdi) {
+        this.eczaneAdi = eczaneAdi;
+    }
+
+    public String getEczaneIlAdi() {
+        return eczaneIlAdi;
+    }
+
+    public void setEczaneIlAdi(String eczaneIlAdi) {
+        this.eczaneIlAdi = eczaneIlAdi;
+    }
+
+    public String getEczaneIlceAdi() {
+        return eczaneIlceAdi;
+    }
+
+    public void setEczaneIlceAdi(String eczaneIlceAdi) {
+        this.eczaneIlceAdi = eczaneIlceAdi;
+    }
+
+    public String getEczaneAdres() {
+        return eczaneAdres;
+    }
+
+    public void setEczaneAdres(String eczaneAdres) {
+        this.eczaneAdres = eczaneAdres;
+    }
+
+    public String getTarih() {
+        return tarih;
+    }
+
+    public void setTarih(String tarih) {
+        this.tarih = tarih;
+    }
+
+    public String getBaslangic() {
+        return baslangic;
+    }
+
+    public void setBaslangic(String baslangic) {
+        this.baslangic = baslangic;
+    }
+
+    public String getBitis() {
+        return bitis;
+    }
+
+    public void setBitis(String bitis) {
+        this.bitis = bitis;
+    }
+
+    public String getEnlem() {
+        return enlem;
+    }
+
+    public void setEnlem(String enlem) {
+        this.enlem = enlem;
+    }
+
+    public String getBoylam() {
+        return boylam;
+    }
+
+    public void setBoylam(String boylam) {
+        this.boylam = boylam;
+    }
+
+    public String getGlnNo() {
+        return glnNo;
+    }
+
+    public void setGlnNo(String glnNo) {
+        this.glnNo = glnNo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOlusturmaTarihi() {
+        return olusturmaTarihi;
+    }
+
+    public void setOlusturmaTarihi(String olusturmaTarihi) {
+        this.olusturmaTarihi = olusturmaTarihi;
+    }
+
+    public Object getGuncellemeTarihi() {
+        return guncellemeTarihi;
+    }
+
+    public void setGuncellemeTarihi(Object guncellemeTarihi) {
+        this.guncellemeTarihi = guncellemeTarihi;
+    }
+
+    public Object getSilindi() {
+        return silindi;
+    }
+
+    public void setSilindi(Object silindi) {
+        this.silindi = silindi;
+    }
+
+}
